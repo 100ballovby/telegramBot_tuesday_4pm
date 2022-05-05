c5f55a8de996a3d51cccbc3c67e5df04fe09bc8e
demidraksin <greatraksin@icloud.com>
2022-05-05 17:24:53 +0300

    Adding check github function 🍕

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telebot
+import requests as r
 
 bot = telebot.TeleBot('your-key-here')
 
@@ -11,6 +12,12 @@ def get_text_messages(message):
     elif message.text == '/help':
         answer = 'Напиши "Привет"'
         bot.send_message(message.from_user.id, answer)
+    elif message.text == '/check-github':
+        response = r.get('https://github.com')
+        if response.status_code == 200:
+            bot.send_message(message.from_user.id, 'Доступен!')
+        else:
+            bot.send_message(message.from_user.id, 'Сломался!')
     else:
         answer = 'Я тебя не понимаю!'
         bot.send_message(message.from_user.id, answer)

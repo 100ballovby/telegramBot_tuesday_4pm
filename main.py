d0f0eeb92a319027e50007a428bd4e6bb5ed0178
demidraksin <greatraksin@icloud.com>
2022-05-10 16:44:34 +0300

    Adding event listener for commands start and help

--- main.py
+++ main.py
@@ -1,25 +1,14 @@
 import telebot
 import requests as r
 
-bot = telebot.TeleBot('your-key-here')
+bot = telebot.TeleBot('')
 
 
-@bot.message_handler(content_types=['text'])  # учим бота принимать текстовые сообщения
-def get_text_messages(message):
-    if message.text == 'Привет':
-        answer = 'Привет, как я могу помочь?'
-        bot.send_message(message.from_user.id, answer)  # отправить_сообщение(кому, что)
-    elif message.text == '/help':
-        answer = 'Напиши "Привет"'
-        bot.send_message(message.from_user.id, answer)
-    elif message.text == '/check-github':
-        response = r.get('https://github.com')
-        if response.status_code == 200:
-            bot.send_message(message.from_user.id, 'Доступен!')
-        else:
-            bot.send_message(message.from_user.id, 'Сломался!')
-    else:
-        answer = 'Я тебя не понимаю!'
-        bot.send_message(message.from_user.id, answer)
+@bot.message_handler(commands=['start', 'help'])
+def start(message):
+    with open('texts/hello.txt', encoding='utf8') as file:
+        greeting = file.read()
+    bot.send_message(message.chat.id, greeting)
+
 
 bot.polling(none_stop=True, interval=0)

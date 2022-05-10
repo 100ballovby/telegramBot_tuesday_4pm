60f743c14e454ef47d8429ca49ed448cd6df7c44
demidraksin <greatraksin@icloud.com>
2022-05-10 17:14:25 +0300

    Adding currencies name list

--- coinbase.py
+++ coinbase.py
@@ -3,7 +3,7 @@ import requests as r
 url = 'https://api.coinbase.com/v2/currencies'
 response = r.get(url)
 
-with open('texts/currencies.txt', 'w') as file:
+with open('texts/currencies.txt', 'w', encoding='utf8') as file:
     data = response.json()
     for currency in data['data']:
         file.write(f'{currency["name"]} ({currency["id"]})\n')
